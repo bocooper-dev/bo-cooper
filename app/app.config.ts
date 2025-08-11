581bc0588e3f68e56d70ce8009f6aebe09374424
James (Bo) Cooper <bocooperdev@gmail.com>
2025-08-11 17:55:41 -0400

    replace images

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,59 +1,59 @@
 export default defineAppConfig({
- global: {
-picture: {
-dark: '/images/bo-cooper-branding-1.png',
-light: '/images/bo-cooper-branding-1.png',
-alt: 'My profile picture'
-},
-meetingLink: 'https://cal.com/',
-email: 'hiring@bo-cooper.com',
-available: true
-},
+	global: {
+		picture: {
+			dark: '/images/bo-cooper-branding-1.png',
+			light: '/images/bo-cooper-branding-1.png',
+			alt: 'My profile picture'
+		},
+		meetingLink: 'https://cal.com/',
+		email: 'hiring@bo-cooper.com',
+		available: true
+	},
 
- ui: {
-colors: {
-primary: 'blue',
-neutral: 'neutral'
-}
-},
+	ui: {
+		colors: {
+			primary: 'blue',
+			neutral: 'neutral'
+		}
+	},
 
- uiPro: {
-pageHero: {
-slots: {
-container: 'py-18 sm:py-24 lg:py-32',
-title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
-description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
-}
-}
-},
+	uiPro: {
+		pageHero: {
+			slots: {
+				container: 'py-18 sm:py-24 lg:py-32',
+				title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
+				description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
+			}
+		}
+	},
 
- footer: {
-credits: `Copyright © ${new Date().getFullYear()}`,
-colorMode: false,
-links: [{
-'icon': 'i-simple-icons-nuxtdotjs',
-'to': 'https://nuxt.com',
-'target': '_blank',
-'aria-label': 'Nuxt Website'
-}, {
-'icon': 'i-simple-icons-discord',
-'to': 'https://discord.com/invite/ps2h6QT',
-'target': '_blank',
-'aria-label': 'Nuxt UI on Discord'
-}, {
-'icon': 'i-simple-icons-x',
-'to': 'https://x.com/nuxt_js',
-'target': '_blank',
-'aria-label': 'Nuxt on X'
-}, {
-'icon': 'i-simple-icons-github',
-'to': 'https://github.com/nuxt/ui',
-'target': '_blank',
-'aria-label': 'Nuxt UI on GitHub'
-}]
-},
+	footer: {
+		credits: `Copyright © ${new Date().getFullYear()}`,
+		colorMode: false,
+		links: [{
+			'icon': 'i-simple-icons-nuxtdotjs',
+			'to': 'https://nuxt.com',
+			'target': '_blank',
+			'aria-label': 'Nuxt Website'
+		}, {
+			'icon': 'i-simple-icons-discord',
+			'to': 'https://discord.com/invite/ps2h6QT',
+			'target': '_blank',
+			'aria-label': 'Nuxt UI on Discord'
+		}, {
+			'icon': 'i-simple-icons-x',
+			'to': 'https://x.com/nuxt_js',
+			'target': '_blank',
+			'aria-label': 'Nuxt on X'
+		}, {
+			'icon': 'i-simple-icons-github',
+			'to': 'https://github.com/nuxt/ui',
+			'target': '_blank',
+			'aria-label': 'Nuxt UI on GitHub'
+		}]
+	},
 
- seo: {
-	 siteName: 'Bo Cooper'
+	seo: {
+		siteName: 'Bo Cooper'
 	}
 })
